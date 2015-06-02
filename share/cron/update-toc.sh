ddc910c66df585a115bfd19359af3b464054cfcb
Snakevil Zen <zsnakevil@gmail.com>
2015-06-02 20:55:15 +0800

    简化自动更新地计划任务。

--- share/cron/update-toc.sh
+++ share/cron/update-toc.sh
@@ -10,32 +10,12 @@ LOG=log/`'basename' "$0" .sh`.log
 
 cd `'dirname' "$0"`/../../var;
 
+'rm' -fr cache/*
+
 'find' db -mindepth 2 -maxdepth 2 -type f -name 'SOURCE' \
-  | while read fi; do \
-      di=`'dirname' "$fi"`; \
-      ID=`'basename' "$di"`; \
-      fs=''; \
-      fd="db/$ID/toc.xml"; \
-      [ ! -f "$fd" ] || { \
-        fs="$fs "`'basename' "$fd"`; \
-        'rm' -f "$fd"; \
-        'curl' "http://szen.in/n/$ID/" > /dev/null 2>&1; \
-      }; \
-      [ ! -d "cache/$ID" ] || { \
-        fd="cache/$ID/index.html"; \
-        [ ! -f "$fd" ] || { \
-          fs="$fs "`'basename' "$fd"`; \
-          'rm' -f "$fd"; \
-        }; \
-        fo=`cd "db/$ID"; 'basename' $('ls' *.xml 2>/dev/null | 'sort' -rn | 'head' -n1) .xml`; \
-        fd="cache/$ID/$fo.html"; \
-        [ ! -f "$fd" ] || { \
-          fs="$fs "`'basename' "$fd"`; \
-          'rm' -f "$fd"; \
-        }; \
-      }; \
-      [ -z "$fs" ] || { \
-        fs=":$fs"; \
-        echo `'date' '+%FT%T%:z'`" $ID$fs" >> "$LOG"; \
-      } \
-    done
+  | while read id; do
+    id=`'basename' $('dirname' "$id")`;
+    'rm' -f "db/$id/toc.xml";
+    'curl' "http://szen.in/n/$id/" > /dev/null 2>&1;
+    echo `'date' '+%FT%T%:z'`" $id" >> "$LOG";
+  done
