2311f965b345c3690763797478b654569d2b8bd4
Snakevil Zen <zsnakevil@gmail.com>
2016-06-14 11:09:27 +0800

    修正直接访问首页时百页问题。

--- lib/Controller/Index.php
+++ lib/Controller/Index.php
@@ -3,7 +3,7 @@
  * 定义首页控制器。
  *
  * @author    Snakevil Zen <zsnakevil@gmail.com>
- * @copyright © 2014 SZen.in
+ * @copyright © 2016 SZen.in
  * @license   GPL-3.0+
  * @license   CC-BY-NC-ND-3.0
  */
@@ -15,23 +15,22 @@ use snakevil\ccnr2;
 /**
  * 首页控制器。
  *
- * @package snakevil\ccnr2
  * @version 2.0.0
+ *
  * @since   2.0.0
  */
 class Index extends ccnr2\Component\Controller
 {
     /**
      * {@inheritdoc}
-     *
-     * @return ccnr2\View\TOC
      */
-    protected function onGet()
+    protected function onGET()
     {
         if (preg_match('|^[a-z]+=(http://)?(\w+\.)+\w+/.+$|', $this->input['server:QUERY_STRING'])) {
             $o_ctrl = new NovelFound($this->appliance);
 
             return $o_ctrl->act($this->token);
         }
+        $this->output->state(404);
     }
 }
